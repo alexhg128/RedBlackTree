ee6c26c698a34506bf85643164fe8086b477ce4c
Alejandro Hahn <30887815+alexhg128@users.noreply.github.com>
2018-11-09 18:45:43 -0700

    Project Init

---
+++ RedBlackTree/cmake-build-debug/CMakeFiles/RedBlackTree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RedBlackTree.dir/main.cpp.obj"
+  "CMakeFiles/RedBlackTree.dir/Structures/RedBlackTree.cpp.obj"
+  "RedBlackTree.pdb"
+  "RedBlackTree.exe"
+  "RedBlackTree.exe.manifest"
+  "RedBlackTree.lib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RedBlackTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
